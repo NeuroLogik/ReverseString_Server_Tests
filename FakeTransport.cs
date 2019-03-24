c502c10f0b1e89d6fb4884f225ea9a3fca763640
Matteo Velardi <matteovelardi79@gmail.com>
2019-03-24 18:13:41 +0100

    First commit: Tests for Reverse String Server

---
+++ FakeTransport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Test
+{
+    class FakeTransport : ITransport
+    {
+        byte[] data;
+
+        public void Bind(string address, int port)
+        {
+
+        }
+
+        public byte[] Receive()
+        {
+            return data;
+        }
+
+        public bool Send(byte[] data)
+        {
+            return true;
+        }
+
+        public void SetFakeTransportData(byte[] data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ TestServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Server.Test
+{
+    class TestServer
+    {
+        Server server;
+        FakeTransport transport;
+
+        [SetUp]
+        public void Setup()
+        {
+            transport = new FakeTransport();
+            server = new Server(transport);
+        }
+
+        [Test]
+        public void TestDataProcessing()
+        {
+            byte[] data = Encoding.UTF8.GetBytes("Bella pe te");
+            transport.SetFakeTransportData(data);
+            server.SingleStep();
+            Assert.That(new string(Encoding.UTF8.GetChars(server.ReversedData)), Is.EqualTo("et ep alleB"));
+        }
+
+        [Test]
+        public void TestDataLengthMatch()
+        {
+            byte[] data = Encoding.UTF8.GetBytes("Bella pe te");
+            transport.SetFakeTransportData(data);
+            server.SingleStep();
+            Assert.That(server.ReceivedDdata.Length, Is.EqualTo(server.ReversedData.Length));
+        }
+    }
+}
